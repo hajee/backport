2d99432b991f84c376dcb2b083ad767297cc07f4
Bert Hajee <hajee@moretIA.com>
2014-12-29 14:19:18 +0100

    Remove debugger

--- lib/puppet/parser/functions/assert_type.rb
+++ lib/puppet/parser/functions/assert_type.rb
@@ -14,38 +14,37 @@
 #
 # See the documentation for "The Puppet Type System" for more information about types.
 #
-require 'ruby-debug'
-debugger
 
 unless Puppet.version.scan(/^3\.6\..*$/).empty?
 
-module Puppet::Parser::Functions
-  newfunction(:assert_type) do |args|
-    type = args[0]
-    value = args[1]
-    block = args[2]
-    assert_type(type,value, block)
-  end
-end
-
-# @param type [Type] the type the value must be an instance of
-# @param value [Object] the value to assert
-#
-def assert_type(type, value, block=nil)
-  unless Puppet::Pops::Types::TypeCalculator.instance?(type,value)
-    inferred_type = Puppet::Pops::Types::TypeCalculator.infer(value)
-    if block
-      # Give the inferred type to allow richer comparisson in the given block (if generalized
-      # information is lost).
-      #
-      value = block.call(nil, type, inferred_type)
-    else
-      # Do not give all the details - i.e. format as Integer, instead of Integer[n, n] for exact value, which
-      # is just confusing. (OTOH: may need to revisit, or provide a better "type diff" output.
-      #
-      actual = Puppet::Pops::Types::TypeCalculator.generalize!(inferred_type)
-      raise Puppet::ParseError, "assert_type(): Expected type #{type} does not match actual: #{actual}"
+  module Puppet::Parser::Functions
+    newfunction(:assert_type) do |args|
+      type = args[0]
+      value = args[1]
+      block = args[2]
+      assert_type(type,value, block)
     end
   end
-  value
+
+  # @param type [Type] the type the value must be an instance of
+  # @param value [Object] the value to assert
+  #
+  def assert_type(type, value, block=nil)
+    unless Puppet::Pops::Types::TypeCalculator.instance?(type,value)
+      inferred_type = Puppet::Pops::Types::TypeCalculator.infer(value)
+      if block
+        # Give the inferred type to allow richer comparisson in the given block (if generalized
+        # information is lost).
+        #
+        value = block.call(nil, type, inferred_type)
+      else
+        # Do not give all the details - i.e. format as Integer, instead of Integer[n, n] for exact value, which
+        # is just confusing. (OTOH: may need to revisit, or provide a better "type diff" output.
+        #
+        actual = Puppet::Pops::Types::TypeCalculator.generalize!(inferred_type)
+        raise Puppet::ParseError, "assert_type(): Expected type #{type} does not match actual: #{actual}"
+      end
+    end
+    value
+  end
 end
